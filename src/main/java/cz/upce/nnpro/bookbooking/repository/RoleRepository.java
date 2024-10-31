f996f280fc2a829450ae2a0f168472d1b17f5968
Jan Stehno <honzastehno@email.cz>
2024-10-31 09:22:23 +0100

    Added Role repository

---
+++ src/main/java/cz/upce/nnpro/bookbooking/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package cz.upce.nnpro.bookbooking.repository;
+
+import cz.upce.nnpro.bookbooking.entity.Role;
+import cz.upce.nnpro.bookbooking.entity.enums.RoleE;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleE name);
+}
