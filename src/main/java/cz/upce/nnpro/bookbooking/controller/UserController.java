24cac5a4afb490873781b9be8980fd8af428c1fe
Jan Stehno <honzastehno@email.cz>
2024-11-01 16:21:51 +0100

    Updated Jwt service token data extraction

--- src/main/java/cz/upce/nnpro/bookbooking/controller/UserController.java
+++ src/main/java/cz/upce/nnpro/bookbooking/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService service;
@@ -23,75 +23,54 @@ public class UserController {
 
     private final PasswordEncoder passwordEncoder;
 
-    @GetMapping("/{id}")
+    @GetMapping
     public ResponseEntity<User> getUserById(
-            @PathVariable
-            Long id,
             @RequestHeader("Authorization")
             String token) {
-        if (modifiable(token, id)) {
-            final User user = service.getById(id);
-            if (user != null) return ResponseEntity.ok(user);
-            return ResponseEntity.notFound().build();
-        }
+        final User user = service.getById(jwtService.extractUserId(token));
+        if (user != null) return ResponseEntity.ok(user);
         return ResponseEntity.status(403).build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping
     public ResponseEntity<?> updateUser(
-            @PathVariable
-            Long id,
             @RequestBody
             @Valid
             UserNameDTO data,
             @RequestHeader("Authorization")
             String token) {
         final User foundByEmail = service.getByEmail(data.getEmail());
-        if (foundByEmail != null && !foundByEmail.getId().equals(id)) {
+        if (foundByEmail != null && !foundByEmail.getId().equals(jwtService.extractUserId(token))) {
             return new ResponseEntity<>(new Exception("EMAIL_EXISTS"), HttpStatus.CONFLICT);
         }
-        if (modifiable(token, id)) {
-            final User user = service.getById(id);
-            if (user != null) {
-                user.setFirstname(user.getFirstname());
-                user.setLastname(user.getLastname());
-                user.setEmail(user.getEmail());
-                service.update(user);
-                return ResponseEntity.ok(jwtService.generateToken(user));
-            }
-            return ResponseEntity.notFound().build();
+        final User user = service.getById(jwtService.extractUserId(token));
+        if (user != null) {
+            user.setFirstname(user.getFirstname());
+            user.setLastname(user.getLastname());
+            user.setEmail(user.getEmail());
+            service.update(user);
+            return ResponseEntity.ok(jwtService.generateToken(user));
         }
         return ResponseEntity.status(403).build();
     }
 
-    @PutMapping("/password/{id}")
+    @PutMapping("/password")
     public ResponseEntity<?> updateUserPassword(
-            @PathVariable
-            Long id,
             @RequestBody
             @Valid
             UserPasswordDTO data,
             @RequestHeader("Authorization")
             String token) {
-        if (modifiable(token, id)) {
-            final User user = service.getById(id);
-            if (user != null) {
-                if (passwordEncoder.matches(data.getOldPassword(), user.getPassword())) {
-                    user.setPassword(passwordEncoder.encode(data.getPassword()));
-                    service.update(user);
-                    return ResponseEntity.ok().build();
-                } else {
-                    return new ResponseEntity<>(new Exception("OLD_PASSWORD_INCORRECT"), HttpStatus.CONFLICT);
-                }
+        final User user = service.getById(jwtService.extractUserId(token));
+        if (user != null) {
+            if (passwordEncoder.matches(data.getOldPassword(), user.getPassword())) {
+                user.setPassword(passwordEncoder.encode(data.getPassword()));
+                service.update(user);
+                return ResponseEntity.ok().build();
+            } else {
+                return new ResponseEntity<>(new Exception("OLD_PASSWORD_INCORRECT"), HttpStatus.CONFLICT);
             }
-            return ResponseEntity.notFound().build();
         }
         return ResponseEntity.status(403).build();
     }
-
-    private boolean modifiable(String token, Long userId) {
-        return userId.equals(jwtService.extractUserId(token.substring(7)));
-    }
-
 }
-
